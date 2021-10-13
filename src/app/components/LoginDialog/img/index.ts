c119a23fbc49ad0e9d87e00f8a60462954214771
jaaia <go787888@gmail.com>
2021-10-13 10:15:56 +0800

    feat: modify svg

--- src/app/components/LoginDialog/img/index.ts
+++ src/app/components/LoginDialog/img/index.ts
@@ -1,12 +1,15 @@
-import authereumIcon from './authereumIcon.svg'
+/* eslint-disable */
+import authereumIcon from './icon.authereumIcon.svg'
 import browserWallets from './browserWallets.png'
-import coinbaseIcon from './coinbaseIcon.svg'
-import formaticIcon from './formaticIcon.svg'
-import imToken from './imToken.svg'
-import ledgerIcon from './ledgerIcon.svg'
-import MEWIcon from './MEWIcon.svg'
-import portisIcon from './portisIcon.svg'
-import torusIcon from './torusIcon.svg'
-import walletConnectIcon from './walletConnectIcon.svg'
+import coinbaseIcon from './icon.coinbaseIcon.svg'
+import formaticIcon from './icon.formaticIcon.svg'
+import imToken from './icon.imToken.svg'
+import ledgerIcon from './icon.ledgerIcon.svg'
+import MEWIcon from './icon.MEWIcon.svg'
+import portisIcon from './icon.portisIcon.svg'
+import torusIcon from './icon.torusIcon.svg'
+import walletConnectIcon from './icon.walletConnectIcon.svg'
+
+console.log(authereumIcon)
 
 export { authereumIcon, browserWallets, coinbaseIcon, formaticIcon, imToken, ledgerIcon, MEWIcon, portisIcon, torusIcon, walletConnectIcon }
